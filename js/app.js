b693619dfc7e6657175c01a299902ef39fb67e12
nboyce <boycenobukho@gmail.com>
2025-05-31 22:33:50 +0200

    function for clicking on the sign up btns

--- js/app.js
+++ js/app.js
@@ -1,10 +1,20 @@
 const signup = document.getElementById("racer-form");
-signup.addEventListener("click", function(){
+
+signup.addEventListener("click", function() {
+    alert("sumn' happened");
+
     const form = document.createElement("form");
-        form.innerHTML = `
-            <label>
-                <input type="checkbox">
-            </label>
-            <button class="submit-btn">Submit</button>
-            `;
-})
+    form.innerHTML = `
+        <label>
+            <input type="text" name="name" placeholder="Enter your name">
+            <input type="text" name="surname" placeholder="Enter your surname">
+            <input type="number" name="age" placeholder="Enter your age">
+            <input type="email" name="email" placeholder="Enter your email address">
+        </label>
+        <br>
+        <button class="submit-btn">Submit</button>
+    `;
+
+    // Append form below the button or wherever you'd like
+    signup.parentElement.appendChild(form);
+});
